6af056d10da4e6678f6ea01d630129069cdf0952
Samuel <samuel.karabetian@gmail.com>
2020-02-28 18:46:15 +0100

    Pushing deleted files

--- Unity-Notifications/Assets/Scripts/MessageSystem/MessageDisplayerSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "Message Displayer/Message Displayer Settings")]
-public class MessageDisplayerSettings : ScriptableObject
-{
-    [Header("Canvas Settings")]
-    public Vector2 _referenceResolution = new Vector2(1920f, 1080f);
-}
