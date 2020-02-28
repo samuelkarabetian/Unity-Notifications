0f90c0468fba17ee5d8874f37a593e25ac61cb0c
Samuel <samuel.karabetian@gmail.com>
2020-02-28 18:29:31 +0100

    Pushed scripts

---
+++ Unity-Message-System/Assets/Scripts/MessageSystem/Message.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Message Displayer/Message")]
+public class Message : ScriptableObject
+{
+    [TextArea(5, 100)]
+    public string _text = "Insert text here";
+    public float _delayInSeconds = 0f;
+    public Vector2 _normalizedPosition = Vector2.one * 0.5f;
+    public MessageStyle _style = null;
+}
---
+++ Unity-Message-System/Assets/Scripts/MessageSystem/MessageStyle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Message Displayer/Message Style Settings")]
+public class MessageStyle : ScriptableObject
+{
+    public Font _font = null;
+    public int _fontSize = 16;
+    public Color _textColor = Color.black;
+    public bool _hasDuration = true;
+    [Range(0f, 10f)] public float _duration = 0f;
+    [Range(0f, 3f)] public float _fadeInDuration = 0f;
+    [Range(0f, 3f)] public float _fadeOutDuration = 0f;
+}
